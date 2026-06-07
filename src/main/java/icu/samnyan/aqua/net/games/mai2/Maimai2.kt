898939485c0e9bd598ab2ee2df6bf3a6b6e69f80
Raymond <101374892+raymonable@users.noreply.github.com>
2026-06-07 00:00:11 -0400

    fix: enforce fullwidth

--- src/main/java/icu/samnyan/aqua/net/games/mai2/Maimai2.kt
+++ src/main/java/icu/samnyan/aqua/net/games/mai2/Maimai2.kt
@@ -40,7 +40,7 @@ class Maimai2(
     // Only show > S rank
     override val shownRanks = mai2Scores.filter { it.first >= 97 * 10000 }
     override val settableFields: Map<String, (Mai2UserDetail, String) -> Unit> by lazy { mapOf(
-        "userName" to usernameCheck(SEGA_USERNAME_CHARS),
+        "userName" to usernameCheck(false),
         "iconId" to { u, v -> u.iconId = v.int() },
         "plateId" to { u, v -> u.plateId = v.int() },
         "titleId" to { u, v -> u.titleId = v.int() },

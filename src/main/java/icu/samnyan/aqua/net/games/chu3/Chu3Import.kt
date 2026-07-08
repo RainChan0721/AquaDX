7ea5add4072bcedc8d0d84790071f26a36ee33b6
Raymond <101374892+raymonable@users.noreply.github.com>
2026-07-08 12:00:02 -0400

    fix: import/export fixes for chu3 & mai2

--- src/main/java/icu/samnyan/aqua/net/games/chu3/Chu3Import.kt
+++ src/main/java/icu/samnyan/aqua/net/games/chu3/Chu3Import.kt
@@ -64,9 +64,10 @@ data class Chu3DataExport(
     var userMapList: List<UserMap>,
     var userMusicDetailList: List<UserMusicDetail>,
     var userPlaylogList: List<UserPlaylog>,
-    var userGeneralDataList: List<UserGeneralData>,
-    var userMiscList: Chu3UserMisc
+    var userMateList: List<UserMate> = emptyList(),
+    var userGeneralDataList: List<UserGeneralData> = emptyList(),
+    var userMiscList: Chu3UserMisc = Chu3UserMisc(),
 ): IExportClass<Chu3UserData> {
     constructor() : this("SDHD",
-        Chu3UserData(), UserGameOption(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), Chu3UserMisc())
+        Chu3UserData(), UserGameOption(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), ArrayList(), Chu3UserMisc())
 }

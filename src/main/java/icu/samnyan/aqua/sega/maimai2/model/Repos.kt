08477c8df9912f1d4daa1e0892b4d51b58cc9f3e
RainChan0721 <yeyulinxi0144@gmail.com>
2026-08-27 04:13:28 +0900

    fix(maimai2): add missing import for Query annotation in Repos.kt

--- src/main/java/icu/samnyan/aqua/sega/maimai2/model/Repos.kt
+++ src/main/java/icu/samnyan/aqua/sega/maimai2/model/Repos.kt
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
